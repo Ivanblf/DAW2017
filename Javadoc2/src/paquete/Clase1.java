74d7d3ff4c3b5bb9771a57600d491f8a1085bb9f
Vespertino <Vespertino@192.168.128.129>
2017-03-07 18:41:51 +0100

    Prueba

--- Javadoc2/src/paquete/Clase1.java
+++ Javadoc2/src/paquete/Clase1.java
@@ -37,8 +37,9 @@ public class Clase1 {
 	public void metodo1(){
 		
 		for (int i = 0; i < atributo1.size(); i++){
+			//Cambiar esto
 			
-			System.out.println(i + ") " + atributo1.get(i));
+			System.out.println(i +1+ ") " + atributo1.get(i));
 		}
 	}
 	
